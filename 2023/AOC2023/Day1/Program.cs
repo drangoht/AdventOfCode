a4c0f79a8b2cc99044e519ced1076d499ae14544
Grégory Thognard <drangoht@gmail.com>
2023-12-01 13:34:52 +0100

    init

---
+++ 2023/AOC2023/Day1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day1;
+using Day1.Tools;
+
+int resultCode = 0;
+foreach (string line in File.ReadLines(@"Input.txt"))
+{
+    resultCode += Convert.ToInt32(Tools.GetNumberResultFromLine(line));
+    Console.WriteLine($"line:{line}  result:{resultCode}");
+}
+Console.WriteLine($"Code:{resultCode}");
+Console.ReadLine();
+
