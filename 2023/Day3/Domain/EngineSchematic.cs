2d58d5b28f6456a7cd61eda14377a2f5274994d1
Grégory Thognard <drangoht@gmail.com>
2023-12-03 12:28:20 +0100

    en cours de dev

---
+++ 2023/Day3/Domain/EngineSchematic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3.Domain
+{
+    public class EngineSchematic
+    {
+        List<Part> Parts = [];
+        public void AddPartsFromString(string line, int x)
+        {
+            int y = 0;
+            Part part = new Part();
+            foreach (var car in line) 
+            {
+                if (IsDot(car))
+                    part=CreateDotPart(car,x,y);
+
+
+                Parts.Add(part);
+                y++;
+            }
+            
+        }
+        private Part CreateDotPart(char car,int x,int y) =>
+            new Part()
+            {
+
+                Coordinates.Add(new Coordinate(x, y)),
+                Value = car.ToString(),
+                PartType=PartType.Dot
+            };
+
+        private bool IsNumber(char ch) => Char.IsDigit(ch);
+        private bool IsDot(char ch) => ch == '.';
+    }
+}
---
+++ 2023/Day3/Domain/Part.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3.Domain
+{
+    public class Part
+    {
+        public string Value { get; set; }
+        public PartType PartType { get; set; }
+        public List<Coordinate> Coordinates { get; set; } = [];
+    }
+}
