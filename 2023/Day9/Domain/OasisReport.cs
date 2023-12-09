59342724c3ac9c812fb8b915ee7c07bb044dd601
Grégory Thognard <drangoht@gmail.com>
2023-12-09 15:45:41 +0100

    Day9

---
+++ 2023/Day9/Domain/OasisReport.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9.Domain
+{
+    public class OasisReport
+    {
+        public List<int> ParseLinesToSeriesAndCalcultateNextNumber(List<string> lines)
+        {
+            List<int> results = new List<int>();
+            List<int> serie = new List<int>();
+            foreach (string line in lines)
+            {
+                serie = new List<int>();
+                var arrNumber = line.Split(' ');
+                foreach (var number in arrNumber)
+                {
+                    serie.Add(int.Parse(number));
+                }
+                results.Add(CalculateNextNumber(serie));
+            }
+            return results;
+        }
+
+        public List<int> ParseLinesToSeriesAndCalcultateFirstNumber(List<string> lines)
+        {
+            List<int> results = new List<int>();
+            List<int> serie = new List<int>();
+            foreach (string line in lines)
+            {
+                serie = new List<int>();
+                var arrNumber = line.Split(' ');
+                foreach (var number in arrNumber)
+                {
+                    serie.Add(int.Parse(number));
+                }
+                serie.Reverse();
+                results.Add(CalculateNextNumber(serie));
+            }
+            return results;
+        }
+        private int CalculateNextNumber(List<int> numbers)
+        {
+            if (numbers.All(x => x == 0))
+                return 0;
+            if(numbers.Count == 1) 
+                return numbers[0];
+            if (numbers.Count == 0) 
+                return 0;
+
+            List<int> newNumbersLine = new List<int>();
+            for (int i = 0; i < numbers.Count-1; i++)
+            {
+                newNumbersLine.Add(numbers[i+1] - numbers[i]);
+            }
+            int nextNumber = CalculateNextNumber(newNumbersLine);
+            return numbers.Last() + nextNumber;
+        }
+    }
+}
---
+++ 2023/Day9/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Day9.Domain;
+
+OasisReport oReport = new OasisReport();
+var numbers = oReport.ParseLinesToSeriesAndCalcultateNextNumber(File.ReadLines(@"Inputs\Input.txt").ToList());
+
+var result1 = numbers.Sum();
+Console.WriteLine(result1);
+
+numbers = oReport.ParseLinesToSeriesAndCalcultateFirstNumber(File.ReadLines(@"Inputs\Input.txt").ToList());
+
+var result2 = numbers.Sum();
+Console.WriteLine(result2);
+Console.ReadLine();
