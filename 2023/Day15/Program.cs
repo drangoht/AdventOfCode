6a3808098035d69388b8a54285354af9aaa8ffa8
Grégory Thognard <drangoht@gmail.com>
2023-12-15 08:07:58 +0100

    Day15 part1

---
+++ 2023/Day15/Program.cs
@@ -0,0 +1,15 @@
+﻿var lines = File.ReadAllLines(@"Inputs\Input.txt");
+var result = 0;
+var tot = 0;
+foreach (var seq in lines[0].Split(","))
+{
+    result = 0;
+    foreach (var car in seq)
+    {
+        var asc = (int)car;
+        result = ((result + asc) * 17) % 256;
+    }
+    tot += result;
+}
+Console.WriteLine(tot);
+Console.ReadLine();
