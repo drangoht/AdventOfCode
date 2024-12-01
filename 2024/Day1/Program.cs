f490b3313b9a7535bc265d91c90f8252295bf4e5
Drangoht <drangoht@gmail.co>
2024-12-01 21:20:19 +0100

    2024/Day1: Solution for AdventOfCode 2024 Day1

---
+++ 2024/Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Day1.Tools;
+
+int difference = 0;
+List<int> colOne = new List<int>();
+List<int> colTwo = new List<int>();
+foreach (string line in File.ReadLines(@"Inputs\Input.txt"))
+{
+    colOne.Add(Int32.Parse(line.Split("  ")[0]));
+    colTwo.Add(Int32.Parse(line.Split("  ")[1]));
+
+    difference = Tools.GetTotalDistance(colOne, colTwo);
+}
+Console.WriteLine($"Difference:{difference}");
+Console.ReadLine();
+
---
+++ 2024/Day1/Tools.cs
@@ -0,0 +1,17 @@
+﻿namespace Day1.Tools
+{
+    public static class Tools
+    {
+        public static int GetTotalDistance(List<int> colOne, List<int> colTwo)
+        {
+            var orderedColOne = colOne.OrderBy(n => n).ToList();
+            var orderedColTwo = colTwo.OrderBy(n => n).ToList();
+            int difference = 0;
+            for(int rank=0;rank<orderedColOne.Count;rank++)
+            {
+                difference += Math.Abs(orderedColOne[rank] - orderedColTwo[rank]);
+            }
+            return difference;
+        }
+    }
+}
