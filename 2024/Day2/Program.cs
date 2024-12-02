a2381299dd0db241a9e7064c3ed3a2ec236ae305
Drangoht <drangoht@gmail.co>
2024-12-02 21:28:44 +0100

    Day2 done !

---
+++ 2024/Day2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Day2.Tools;
+
+
+int safeReports = 0;
+foreach (string line in File.ReadLines(@"Inputs\Input.txt"))
+{
+    var reports = line.Split(' ').Select(n => Int32.Parse(n)).ToList();
+    if( Tools.IsSafe(reports))
+        safeReports++;
+}
+Console.WriteLine($"Difference:{safeReports}");
+Console.ReadLine();
+
---
+++ 2024/Day2/Tools.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Day2.Tools
+{
+    public static class Tools
+    {
+        public static bool IsSafe(List<int>? reports)
+        {
+            var prec = -1;
+            var precOrder = 0;
+            
+            foreach(var r in reports)
+            {
+                if(prec != -1)
+                {
+                    var diff = Math.Abs(r - prec); 
+                    if (diff > 3)
+                        return false; 
+                    if (diff == 0)
+                        return false;
+                    if (precOrder != 0)
+                    {
+                       
+                        if (precOrder != ((r - prec) / diff))
+                            return false;
+                    }
+                    precOrder = (r - prec) / diff;
+                }
+                prec = r;
+            }
+            return true;
+        }
+    }
+}
